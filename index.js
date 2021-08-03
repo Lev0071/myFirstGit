67091f8e4594a897b78b1c9645da734bff6d1361
Yasin Çakar <coder0071@outlook.com>
2021-08-03 11:30:26 +1000

    added adding function

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const app = express();
 
 const port = 3000;
 
+function myFunction(p1, p2) {
+  return p1 + p2;   // The function returns the product of p1 and p2
+}
+
 app.get("/",(req,res)=>{
-  res.send("Hello World, Workshop 2 - 3813ICT Software Frameworks");
+  res.write("Hello World, Workshop 2 - 3813ICT Software Frameworks");
+  res.write();
+  res.write("The sum is %s",myFunction(5,10));
+  res.send();
 });
 
 app.listen(port,function(){
   console.log("Hello World, Workshop 2 - 3813ICT Software Frameworks");
+  console.log("The sum is %s",myFunction(5,10));
 });
